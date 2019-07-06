43c2ad92312644ed7fe467b9d99a622bb4e56ad9
Eugene-x-Code <evgeni.iv.xy@gmail.com>
2019-07-06 16:30:52 +0300

    hernya

---
+++ src/js/3navigation.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import { isMainThread } from "worker_threads";
+
+const home = document.querySelector('.nav__home');
+const myLibrary = document.querySelector('nav__library');
+
+home.addEventListener('click', activeHomePage);
+
+function activeLibraryPage () {
+  
+}
+
+function activeHomePage () {
+
+}
