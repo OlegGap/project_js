3e52d54e0036415d008d5ba9471f37f2898ce8f0
Eugene-x-Code <evgeni.iv.xy@gmail.com>
2019-07-07 14:47:23 +0300

    navi

--- src/js/3navigation.js
+++ src/js/3navigation.js
@@ -1,12 +1,22 @@
-const home = document.querySelector('.nav__home');
-const myLibrary = document.querySelector('nav__library');
+const homeBtn = document.querySelector('.nav__home');
+const myLibraryBtn = document.querySelector('.nav__library');
+const myLibrary = document.querySelector('.js-library-page');
+const detailsPage = document.querySelector('.js-detail-page');
+const homePage = document.querySelector('.js-home-page');
+const headerLogo = document.querySelector('.header__logo');
 
-home.addEventListener('click', activeHomePage);
+homeBtn.addEventListener('click', activeHomePage);
+myLibraryBtn.addEventListener('click', activeLibraryPage);
+headerLogo.addEventListener('click', activeHomePage);
 
-function activeLibraryPage () {
-  
+function activeLibraryPage() {
+  homePage.style.display = 'none';
+  myLibrary.style.display = 'block';
+  detailsPage.style.dispaly = 'none';
 }
 
-function activeHomePage () {
-
+function activeHomePage() {
+  homePage.style.display = 'block';
+  myLibrary.style.dispaly = 'none';
+  detailsPage.style.dispaly = 'none';
 }
