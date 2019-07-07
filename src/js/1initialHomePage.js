753faebfaf4db1b5241f84aefe236c0675f672ff
Andrii Korniienko <andrii.ds.kor@gmail.com>
2019-07-07 09:42:36 +0300

    buttons and field for the searching

--- src/js/1initialHomePage.js
+++ src/js/1initialHomePage.js
@@ -1,5 +1,38 @@
-'use strict'
+'use strict';
 
-const test = document.querySelector('.test');
+const renderFilms = [];
+const genres = [];
+const pageNumber = 1;
+const list = document.querySelector(".cards__container");
 
-console.log(test);
+function createCardFunc(imgPath, filmTitle, movieId ) {
+
+  const li = document.createElement('li');
+  li.className = 'card__container';
+  li.setAttribute('data-id', movieId);
+  
+  const divMark = document.createElement('div');
+  divMark.className = 'card__mark';
+
+  const divTitle = document.createElement('div');
+  divTitle.className = 'card__title';
+  divTitle.innerHTML = filmTitle;
+
+  const divImg = document.createElement('div')
+  divImg.className = 'card__img';
+  divImg.innerHTML = imgPath;
+
+  li.append(divMark, divTitle, divImg);
+  list.appendChild(li);
+}
+
+function fetchPopularMoviesList() {
+  fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=56d683041d5d1a0178e72b4a2ffc8e86&language=en-US&page=${pageNumber}`,
+  )
+    .then(res => res.json())
+    .then(data => console.log(data))
+   
+} 
+
+fetchPopularMoviesList()
